6cfff06273932e1d55e8992a107dca8014bf6bef
siddhesh-hit <siddhesh@handsintechnology.com>
2024-02-10 17:05:45 +0530

    modified : redux

--- src/Routefiles.jsx
+++ src/Routefiles.jsx
@@ -11,27 +11,28 @@ import DebateDetails from "./pages/DebateDetails";
 import Homepage1 from "./pages/Homepage1";
 import Debate from "./pages/Debate";
 import Homepage2 from "./pages/Homepage2";
+
 const Routesfile = () => {
-    return (
-        <div>
-            <BrowserRouter>
-                <Header />
-                <Routes>
-                    <Route path="/" element={<Homepage />} />
-                    <Route path="/Homepage1" element={<Homepage1 />} />
-                    <Route path="/SearchDetails" element={<SearchDetails />} />
-                    <Route path="/Library" element={<Library />} />
-                    <Route path="/AboutUs" element={<AboutUs />} />
-                    <Route path="/ContactUs" element={<ContactUs />} />
-                    <Route path="/HelpDesk" element={<HelpDesk />} />
-                    <Route path="/DebateDetails" element={<DebateDetails />} />
-                    <Route path="/Debate" element={<Debate />} />
-                    <Route path="/Homepage2" element={<Homepage2 />} />
-                </Routes>
-                <Footer />
-            </BrowserRouter>
-        </div>
-    );
+  return (
+    <div>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/Homepage1" element={<Homepage1 />} />
+          <Route path="/SearchDetails" element={<SearchDetails />} />
+          <Route path="/Library" element={<Library />} />
+          <Route path="/AboutUs" element={<AboutUs />} />
+          <Route path="/ContactUs" element={<ContactUs />} />
+          <Route path="/HelpDesk" element={<HelpDesk />} />
+          <Route path="/DebateDetails" element={<DebateDetails />} />
+          <Route path="/Debate" element={<Debate />} />
+          <Route path="/Homepage2" element={<Homepage2 />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default Routesfile;
