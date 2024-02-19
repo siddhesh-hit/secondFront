cfaafe72feeb477b2f85a7b7ddc80d9dde1f74e1
siddhesh-hit <siddhesh@handsintechnology.com>
2024-02-19 18:23:48 +0530

    Login

--- src/Routefiles.jsx
+++ src/Routefiles.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Homepage from "./pages/Homepage";
-import Header from "./components/common/Header";
-import Footer from "./components/common/Footer";
+// import Header from "./components/common/Header";
+// import Footer from "./components/common/Footer";
 import SearchDetails from "./pages/SearchDetails";
 import Library from "./pages/Library";
 import AboutUs from "./pages/AboutUs";
@@ -11,12 +11,13 @@ import DebateDetails from "./pages/DebateDetails";
 import Homepage1 from "./pages/Homepage1";
 import Debate from "./pages/Debate";
 import Homepage2 from "./pages/Homepage2";
+import Login from "./pages/Login";
 
 const Routesfile = () => {
   return (
     <div>
       <BrowserRouter>
-        <Header />
+        {/* <Header /> */}
         <Routes>
           <Route path="/" element={<Homepage />} />
           <Route path="/Homepage1" element={<Homepage1 />} />
@@ -28,8 +29,9 @@ const Routesfile = () => {
           <Route path="/DebateDetails" element={<DebateDetails />} />
           <Route path="/Debate" element={<Debate />} />
           <Route path="/Homepage2" element={<Homepage2 />} />
+          <Route path="/Login" element={<Login />} />
         </Routes>
-        <Footer />
+        {/* <Footer /> */}
       </BrowserRouter>
     </div>
   );
